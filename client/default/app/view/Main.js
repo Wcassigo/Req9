10d9a9fa18bbd1d58f8744cb87707555077fb347
Wagner Cassigoli <wagner.cassigoli@networkrail.co.uk>
2013-05-14 14:03:33 +0100

    test 1
    there it goes

--- client/default/app/view/Main.js
+++ client/default/app/view/Main.js
@@ -23,7 +23,7 @@ Ext.define('Req9.view.Main', {
                 },
 
                 html: [
-                    "Wagner ----- You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
+                    "Wagner 1----- You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
                     "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
                     "and refresh to change what's rendered here."
                 ].join("")
